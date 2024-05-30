62e99b0f5b1f74cfcd1c16776b1789a771eff159
RomanKuschanow <kuschanow@gmail.com>
2024-05-30 19:01:39 +0300

    CustomRule (not tested)

---
+++ Lexer/Rules/CustomRule.cs
@@ -0,0 +1,31 @@
+﻿#nullable disable
+
+namespace Lexer.Rules;
+public class CustomRule : IRule
+{
+    private string _type;
+    private Func<string, CancellationToken, Task<AnalyzedLayer>> _func;
+    public string Type
+    {
+        get => _type;
+        set => _type = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public bool IsIgnored { get; set; }
+    public bool IsEnabled { get; set; }
+
+    public Func<string, CancellationToken, Task<AnalyzedLayer>> Func
+    {
+        get => _func;
+        set => _func = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public CustomRule(Func<string, CancellationToken, Task<AnalyzedLayer>> func, string type, bool isIgnored = false, bool isEnabled = true)
+    {
+        Func = func;
+        Type = type;
+        IsIgnored = isIgnored;
+        IsEnabled = isEnabled;
+    }
+
+    public async Task<AnalyzedLayer> FindLexemes(string str, CancellationToken ct) => await Func(str, ct);
+}
