161544cba8bd7dbb4d7f20b5ec041835f46c9a93
RomanKuschanow <kuschanow@gmail.com>
2024-05-30 19:17:55 +0300

    WordsRule (not tested)

---
+++ Lexer/Rules/WordsRule.cs
@@ -0,0 +1,39 @@
+﻿#nullable disable
+
+using System.Text.RegularExpressions;
+
+namespace Lexer.Rules;
+public class WordsRule : IRule
+{
+    private string _type;
+    private IEnumerable<string> _words;
+
+    public string Type
+    {
+        get => _type;
+        set => _type = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public bool IsIgnored { get; set; }
+    public bool IsEnabled { get; set; }
+
+    public IEnumerable<string> Words
+    {
+        get => _words;
+        set => _words = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public WordsRule(IEnumerable<string> words, string type, bool isIgnored = false, bool isEnabled = true)
+    {
+        Words = words;
+        Type = type;
+        IsIgnored = isIgnored;
+        IsEnabled = isEnabled;
+    }
+
+    public async Task<AnalyzedLayer> FindLexemes(string str, CancellationToken ct)
+    {
+        var matches = Regex.Matches(str, $"\b{string.Join('|', Words)}\b");
+
+        return await Task.FromResult(new AnalyzedLayer(matches.Select(m => new RawLexeme(m.Index, m.Length, this))));
+    }
+}
