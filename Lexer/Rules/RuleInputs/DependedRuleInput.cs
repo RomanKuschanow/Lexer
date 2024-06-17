1480f1796a01a525e42dcceee4094e67aa25b825
RomanKuschanow <kuschanow@gmail.com>
2024-06-17 13:13:49 +0300

    fix DependedRuleInput

---
+++ Lexer/Rules/RuleInputs/DependedRuleInput.cs
@@ -0,0 +1,14 @@
+﻿using Lexer.Rules.Interfaces;
+using Lexer.Rules.RawResults;
+using System.Collections.Immutable;
+
+namespace Lexer.Rules.RuleInputs;
+public class DependedRuleInput : RuleInput, IDependedRuleInput
+{
+    public ImmutableDictionary<IRule, AnalyzedLayer> Dependencies { get; init; }
+
+    public DependedRuleInput(string text, IDictionary<IRule, AnalyzedLayer> dependencies) : base(text)
+    {
+        Dependencies = dependencies.ToImmutableDictionary();
+    }
+}
--- Lexer/Rules/Visitors/Visitor.cs
+++ Lexer/Rules/Visitors/Visitor.cs
@@ -6,12 +6,12 @@ namespace Lexer.Rules.Visitors;
 public class Visitor : IVisitor
 {
     public IRuleInput Rule(VisitorInput visitorInput) => new RuleInput(visitorInput.Text);
-    public IDependedRuleInput DependencyRule(VisitorInput visitorInput, IDependedRule rule)
+    public IDependencyRuleInput DependencyRule(VisitorInput visitorInput, IDependencyRule rule)
     {
         Dictionary<IRule, AnalyzedLayer> dependencies = new();
         foreach (IRule dependencyRule in rule.Dependencies.Keys)
             dependencies.Add(dependencyRule, visitorInput.Layers[dependencyRule]);
 
-        return new DependencyRuleInput(visitorInput.Text, dependencies);
+        return new DependedRuleInput(visitorInput.Text, dependencies);
     }
 }
